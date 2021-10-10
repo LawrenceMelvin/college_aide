c92433910fd03f2751e24b324661c1c80219d507
Lawrence <lawrence2000april@gmail.com>
2021-10-10 08:42:33 +0530

    final setup

--- awareness/forms.py
+++ awareness/forms.py
@@ -2,5 +2,5 @@ from django import forms
 PERSON = [('student','Student'),('staff','Staff')]
 WANTED = [('placement','Placement'),('job','Job'),('skill','Skill'),('english','English')]
 class awareform(forms.Form):
-    person = forms.ChoiceField(choices=PERSON)
-    wanted = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=WANTED)
+    person = forms.ChoiceField(choices=PERSON,)
+    wanted = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={'class':'form-check-input'}), choices=WANTED)
